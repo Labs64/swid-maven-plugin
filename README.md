b2f4449e6cea4d80df4dc105e62440e418f2fa0a
R.Brown <ready.brown@hotmail.de>
2014-12-29 21:23:10 +0100

    - init project structure

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 Generate SoftWare IDentification (SWID) Tags according to [ISO/IEC 19770-2:2009](http://www.iso.org/iso/home/store/catalogue_tc/catalogue_detail.htm?csnumber=53670)
 
-### Quick Start
+## Quick Start
 
 The recommended way to get started using `swid-maven-plugin` in your project is with a plugin management system – the Maven snippet below can be copied and pasted into your build.
 
 ```
 <plugins>
   <plugin>
-    <groupId>com.labs64.utils</groupId>
+    <groupId>com.labs64.mojo</groupId>
     <artifactId>swid-maven-plugin</artifactId>
     <version>0.1.0</version>
   </plugin>
---
+++ src/main/java/com/labs64/mojo/SwidGeneratorMojo.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.labs64.mojo;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Goal which generates SWID Tag for a given POM.
+ */
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
+public class SwidGeneratorMojo extends AbstractMojo {
+    /**
+     * Location of the SWID tags.
+     */
+    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
+    private File outputDirectory;
+
+    public void execute() throws MojoExecutionException {
+        getLog().info("Generate SWID Tag...");
+    }
+
+}
