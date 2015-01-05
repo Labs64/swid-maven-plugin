daadd4d251c3e1bdf6044497c83eb7276446e1d7
R.Brown <ready.brown@hotmail.de>
2015-01-05 13:04:52 +0100

    - use SWID Tags Generator for the swidtag file creation

--- src/main/java/com/labs64/mojo/swid/GenerateMojo.java
+++ src/main/java/com/labs64/mojo/swid/GenerateMojo.java
@@ -16,15 +16,19 @@
 package com.labs64.mojo.swid;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.iso.standards.iso._19770.__2._2009.schema.SoftwareIdentificationTagComplexType;
 
+import com.labs64.utils.swid.SwidBuilder;
+import com.labs64.utils.swid.io.SwidWriter;
+import com.labs64.utils.swid.processor.DefaultSwidProcessor;
+import com.labs64.utils.swid.processor.SwidProcessor;
 import com.labs64.utils.swid.support.SwidUtils;
 
 /**
@@ -33,47 +37,60 @@ import com.labs64.utils.swid.support.SwidUtils;
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
 public class GenerateMojo extends AbstractMojo {
 
-    /**
-     * Encoding for the generated .swidtag files.
-     */
-    @Parameter(property = "encoding", required = false, defaultValue = "${project.build.sourceEncoding}")
-    private String encoding;
+    @Component
+    private org.apache.maven.project.MavenProject project;
 
     /**
      * Location of the generated SWID tags.
      */
-    @Parameter(property = "outputDirectory", required = true, defaultValue = "${project.build.directory}")
+    @Parameter(property = "swid.outputDirectory", required = true, defaultValue = "${project.build.directory}")
     private File outputDirectory;
 
+    /**
+     * Encoding for the generated .swidtag files.
+     */
+    @Parameter(property = "swid.encoding", required = false, defaultValue = "${project.build.sourceEncoding}")
+    private String encoding;
+
+    /**
+     * Encoding for the generated .swidtag files.
+     */
+    @Parameter(property = "swid.extension", required = false, defaultValue = "swidtag")
+    private String extension;
+
     public void execute() throws MojoExecutionException {
-        getLog().info("Generate SWID Tag...");
+        getLog().debug("Generate SWID Tag...");
 
         File dir = outputDirectory;
-
         if (!dir.exists()) {
             dir.mkdirs();
         }
 
         final String regId = SwidUtils.generateRegId("2010-04", "com.labs64");
-        final String fileName = SwidUtils.generateSwidFileName(regId, "NetLicensing", "NLIC-210");
 
+        final String fileName = SwidUtils.generateSwidFileName(regId,
+                project.getArtifactId(),
+                project.getVersion(),
+                extension);
         File touch = new File(dir, fileName);
 
-        FileWriter w = null;
-        try {
-            w = new FileWriter(touch);
-            w.write(regId);
-        } catch (IOException e) {
-            throw new MojoExecutionException("Error creating file " + touch, e);
-        } finally {
-            if (w != null) {
-                try {
-                    w.close();
-                } catch (IOException e) {
-                    // ignore
-                }
-            }
-        }
+        // prepare SWID Tag processor
+        SwidProcessor processor = new DefaultSwidProcessor();
+        ((DefaultSwidProcessor) processor).setEntitlementRequiredIndicator(true)
+                .setProductTitle(project.getName())
+                .setProductVersion(project.getVersion(), 2, 1, 0, 0)
+                .setSoftwareCreator("Labs64", regId)
+                .setSoftwareLicensor("Labs64", regId)
+                .setSoftwareId("NLIC", regId)
+                .setTagCreator("Labs64", regId);
+
+        // create builder and pass processor as build param
+        SwidBuilder builder = new SwidBuilder();
+        SoftwareIdentificationTagComplexType swidTag = builder.build(processor);
+
+        // output resulting object
+        SwidWriter writer = new SwidWriter();
+        writer.write(swidTag, touch);
     }
 
 }
