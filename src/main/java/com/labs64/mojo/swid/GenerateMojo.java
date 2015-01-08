996cdef7f4138a27565d85fc9e753a9b2cb9cb8b
R.Brown <r-brown@users.noreply.github.com>
2015-01-08 15:00:23 +0100

    fix package name

--- src/main/java/com/labs64/mojo/swid/GenerateMojo.java
+++ src/main/java/com/labs64/mojo/swid/GenerateMojo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package test;
+package com.labs64.mojo.swid;
 
 import java.io.File;
 
